304065e781d2794935a9b907e99611253240538d
gurudarshan266 <gpollep@ncsu.edu>
2017-04-16 02:28:04 -0400

    Added get_myip()

--- Utils.py
+++ Utils.py
@@ -24,5 +24,6 @@ def byte_array_to_int(b_arr):
 def get_my_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect(("8.8.8.8", 80))
-    print(s.getsockname()[0])
+    val = s.getsockname()[0]
     s.close()
+    return val
