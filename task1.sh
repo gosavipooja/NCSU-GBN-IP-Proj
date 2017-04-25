b593ad9eba2e0109b61c4f50cd953d86cec1519d
gurudarshan266 <gpollep@ncsu.edu>
2017-04-24 21:31:07 -0400

    Added complete commands in task1.sh

--- task1.sh
+++ task1.sh
@@ -1,16 +1,27 @@
 #!/bin/sh
-s_ip='127.0.0.1'
+s_ip='152.7.99.61'
 s_port=7735
 filenm='../UT/in.txt'
 
-p='0.0'
-N=10
-MSS=1024
+p='0.05'
+N_arr=(1 2 4 8 16 32 64 128 256 512 1024)
+MSS=500
 
 src=$1
 
 if [[ -n "$src" ]]; then
 	echo "Please provide script location as argument"
+	#exit 0
 fi
 
-python $src $s_ip $s_port $filenm $N $MSS 
+for N in ${N_arr[*]};
+do
+	for i in `seq 1 5`;
+	do
+		python $src $s_ip $s_port $filenm $N $MSS
+		echo "N=$N Iter=$i"
+		#exit 0
+	done
+done
+
+ 
